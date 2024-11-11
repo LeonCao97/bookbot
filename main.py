a1b2a77533225222d7b9be99344e94ce1be0f63a
LeonCao97 <leoncao97@gmail.com>
2024-11-11 01:30:21 -0600

    finishing up bookbot project

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(num_words)
-    count_characters(text)
+
+    final_list = make_list(count_characters(text))
+    final_list.sort(reverse=True, key=sort_on)
+    print_results(final_list, num_words, book_path)  
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -22,6 +26,26 @@ def count_characters(text):
         if not(character in character_results):
             character_results[character] = text_lower.count(character)
     print(character_results)
+    return character_results
 
+def make_list(dictionary_results):
+    results_list = []
+    
+    for key, value in dictionary_results.items():
+        if key in "abcdefghijklmnopqrstuvwxyz":
+            dictionary_entry = {}
+            dictionary_entry["name"] = key
+            dictionary_entry["num"] = value
+            results_list.append(dictionary_entry)  
+    return results_list
 
+def sort_on(dict):
+    return dict["num"]
+
+def print_results(results, count, bookPath):
+    print(f"--- Begin report of {bookPath} ---")
+    print(f"{count} words found in the document")
+    for entries in results:
+        print(f"The \'{entries["name"]}\' was found {entries["num"]} times")
+    print("--- End report ---")    
 main()
