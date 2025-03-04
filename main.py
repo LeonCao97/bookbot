146a30c84ce30cbb0d5b02cd9f2145da43a50ac9
LeonCao97 <leoncao97@gmail.com>
2025-03-03 18:02:31 -0600

    update main.py with accuracte word count function

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def get_book_text(file_path):
     with open(file_path) as f:
         results_string = f.read()
-
-    return results_string
+        results_list = results_string.split()
+    return f"{len(results_list)} words found in the document."
 
 def main():
     print(get_book_text("books/frankenstein.txt"))
