a3bb159c2a13c9ebd95a67109a6939300fca1299
LeonCao97 <LeonCao97@gmail.com>
2025-07-22 20:53:18 -0500

    finished project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import word_count, character_count
+from stats import word_count, character_count, final_results
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,9 +9,19 @@ def get_book_text(filepath):
 
 
 
+
 def main():
-    word_count(get_book_text("books/frankenstein.txt"))
-    character_count(get_book_text("books/frankenstein.txt"))
-    
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        word_count(get_book_text(sys.argv[1]))
+        character_count(get_book_text(sys.argv[1]))
+        final_entries = final_results(character_count(get_book_text(sys.argv[1])))
+        print("----Character Count-----")
+        for entry in final_entries:
+            print(f"{entry["char"]}: {entry["num"]}")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 def word_count(book_text):
     list_words = book_text.split()
     num_words = len(list_words)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
 
 def character_count(book_text):
     str_book_lower = book_text.lower()
@@ -12,3 +12,23 @@ def character_count(book_text):
         else:
             character_stats[char] += 1
     print(character_stats)
+    return(character_stats)
+
+def final_results(initial_dict):
+    final_list = []
+    for key in initial_dict:
+        individual_entry = {}
+        individual_entry["char"] = key
+        individual_entry["num"] = initial_dict[key]
+        final_list.append(individual_entry)
+    #print(final_list)
+    final_list.sort(reverse=True, key=sort_on)
+    return(final_list)
+
+    
+
+def sort_on(items):
+    return items["num"]
+
+
+
